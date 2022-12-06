c7d7b497021b8c77b6cfc13ce9c7d34408650144
Fatemah Mukadum <fm1484@log-3.hpc.nyu.edu>
2022-12-06 16:35:25 -0500

    Current state of week 9

---
+++ Week9/Data/prod.runs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# create a for loop in bash to equilibrate the system
+for i in 1.5 1.0 0.9 0.8 0.7 0.65 0.6 0.55 0.5 0.475
+    do
+	mpirun lmp -var configfile ../Inputs/n360/kalj_n360_T$i.lmp -var id 1 -in ../Inputs/production_3d_binary.lmp
+done
